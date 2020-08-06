6eff25d7906fc2ba1fb802b68c83a8d121d682d0
Renato Fragoso <rof1966@hotmail.com>
2020-08-06 17:54:27 -0300

    first commit

---
+++ aplicacao/rds.conf.php
@@ -0,0 +1,2 @@
+*<?php $RDS_URL='bd-scn.c3dlfe3v0b8w.us-east-1.rds.amazonaws.com'; $RDS_DB='dados'; $RDS_user='admin'; $RDS_pwd='nnf9604*';?>
+
---
+++ aplicacao/s3-conf.php
@@ -0,0 +1,8 @@
+<?php
+$accessKey = 'AKIA4Z6Y6UKWNEFGMEMC';
+$secretKey = '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';
+$region = 'us-east-1';
+$bucket = 'week-cloud';
+$arqName =  'logo.jpg';
+$linkestatico = 'http://week-cloud.s3-website-us-east-1.amazonaws.com/'
+?>
